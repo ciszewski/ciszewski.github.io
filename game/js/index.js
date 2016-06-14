9d3d91aa0fd3c64a00b0ddb7d25360eec88b5df8
ciszewski <rciszewski@connect.kellettschool.com>
2016-06-14 16:08:19 +0800

    game upload

---
+++ game/js/index.js
@@ -0,0 +1,79 @@
+ function playGame () {
+alert("Guess the nba facts!");
+
+var question1= prompt(" Guess where is Tony Parker from! Scotland? Spain? France? Argentina?");
+
+if (question1==="Scotland") {
+alert('Bad luck! You guessed wrong');  
+}
+
+else if (question1 ==="Spain"){
+ alert('Bad luck! You guessed wrong'); 
+}
+
+else if (question1==="France") {
+ alert('Amazing! You guessed right!');
+}
+
+else if  (question1==="Argentina") {
+   alert('Bad luck! You guessed wrong');
+}
+
+
+
+var question2=prompt("What year was Michael Jordan drafted? 1983? 1980? 1984?")
+
+if (question2==="1983") {
+ alert('Bad luck! You guessed wrong');
+}
+
+else if (question2==="1980") {
+   alert('Bad luck! You guessed wrong');
+}
+
+else if (question2==="1984") {
+   alert('Amazing! You guessed right!');
+}
+
+var question3=prompt("Who was drafted first in the 1984 draft? Michael Jordan? Hakeem Olajuwon? Sam Bowie?")
+
+if (question3==="Michael Jordan") {
+   alert('Bad luck! You guessed wrong');
+}
+
+else if (question3==="Hakeem Olajuwon") {
+   alert('Amazing! You guessed right!');
+}
+
+else if (question3==="Sam Bowie") {
+   alert('Bad luck! You guessed wrong');
+}
+
+var question4=prompt("How many points was the most scored in an NBA game? 60? 81? 100?")
+
+if (question4==="60") {
+   alert('Bad luck! You guessed wrong');
+}
+
+else if (question4==="81") {
+   alert('Bad luck! You guessed wrong');
+}
+
+else if (question4==="100") {
+   alert('Amazing! You guessed right!');
+}
+
+var question5=prompt("Who scored the right amount of points? Kareem Adul-Jabbar? Wilt Chamberlain? Michael Jordan?")
+
+if (question5==="Kareem Adul-Jabbar") {
+   alert('Bad luck! You guessed wrong');
+}
+
+else if (question5==="Wilt Chamberlain") {
+   alert('Amazing! You guessed right!');
+}
+
+else if (question5==="Michael Jordan") {
+   alert('Bad luck! You guessed wrong');
+}
+ }
